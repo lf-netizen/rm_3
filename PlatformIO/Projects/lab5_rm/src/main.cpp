8f5d83699d110e5d113bf960ab31dce3292ac714
lf-netizen <81683154+lf-netizen@users.noreply.github.com>
2023-03-29 19:13:13 +0200

    Add files via upload

--- PlatformIO/Projects/lab5_rm/src/main.cpp
+++ PlatformIO/Projects/lab5_rm/src/main.cpp
@@ -1,63 +1,62 @@
 #include <Arduino.h>
 #include <VL53L0X.h>
-#include <Servo.h>
+// standard libraries
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+// external libraries
+#include <PID_v1.h>
+#include <pio_rotary_encoder.h>
 
 
+// Motor encoder output pulses per 360 degree revolution
+#define ENC_COUNT_REV 3840
+// initialise static encoder variables for both motors
+int RotaryEncoder::rotation_motor_a = 0;
 
-// #define buzzer 22
-int io = 22;
-VL53L0X sensor;
-
-Servo servo;  // create servo object to control a servo
-
-int pos ;    // variable to store the servo position
+int RotaryEncoder::rotation_motor_b = 0;
+// create motor object
+RotaryEncoder encoder_left(2, 3, MOTOR_A_SM);
+RotaryEncoder encoder_right(26, 27, MOTOR_B_SM);
 
 
+#define PIN_INPUT 0
+#define PIN_OUTPUT 8
 
-void setup()
-{
-  //inicjalizacja uarta
-  Serial.begin(9600);
-  Wire.begin();
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
 
-  sensor.setTimeout(500);
-  if (!sensor.init())
-  {
-    Serial.println("Failed to detect and initialize sensor!");
-    while (1) {}
-  }
+//Specify the links and initial tuning parameters
+double Kp=1, Ki=0.1, Kd=0.2;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
-  //inicjalizacja czujnika
-  sensor.startContinuous();
-  //serwomechanizm 
-  servo.attach(12);
+
+void setup() {
+  // initialise encoders
+  encoder_left.set_rotation(0);
+  encoder_right.set_rotation(0);
+  SerialUSB.begin(115200);
+  delay(1000);
+
+  Input = encoder_left.get_rotation()/ 4;
+  Setpoint = Input + 30;
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
 }
-
-void read_sensor()
-{
-  Serial.print(sensor.readRangeContinuousMillimeters());
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-  Serial.println();
-
-
-}
-
-void loop()
-{
-  for (pos = 0; pos <= 180; pos += 1) { // rotate from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    servo.write(pos);                   // tell servo to go to position in variable 'pos'
-    read_sensor();
-    delay(20);                          // waits 10ms for the servo to reach the position
-  }
-
-  for (pos = 180; pos >= 0; pos -= 1) { // rotate from 180 degrees to 0 degrees
-    servo.write(pos);                   // tell servo to go to position in variable 'pos'
-    read_sensor();
-    delay(20);                          // waits 10ms for the servo to reach the position
-  }
-
-}
-
-
+void loop() {
+// print encode readings in ticks
+  SerialUSB.print("enc_left_ticks:= ");
+  SerialUSB.println(encoder_left.get_rotation()/ 4);
+  SerialUSB.print("enc_right_ticks:= ");
+  SerialUSB.println(encoder_right.get_rotation()/ 4);
   
+  Input = encoder_left.get_rotation()/ 4;
+  Setpoint = Input + 30; 
+  myPID.Compute();
+  if (Input < 500)
+    analogWrite(PIN_OUTPUT, Output);
+  else
+    analogWrite(PIN_OUTPUT, 0);
+  delay(250);
+}
